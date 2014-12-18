9cb60a2f52a102fe86f3a5791a88ee6182b1f882
Jason Dusek <jason.dusek@gmail.com>
2014-12-18 00:01:28 -0800

    Some conveniences and packaging stuff.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+import * as moment from "moment-timezone";
 import * as React from "react";
 
 import * as Clock from "./lib";
 
-window.Clock = Clock; // "export" for use from console.
+// "export" for use from console.
+window.moment = moment;
+window.React = React;
+window.Clock = Clock;
 
 React.render(<Clock.Clock />, document.body);
